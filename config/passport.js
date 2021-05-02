eab48a80be1d27899ddd4ac8e68f17d489cd464e
Jeremy Seckinger <jtseck@aol.com>
2021-05-02 16:14:24 -0700

    Updated passport.js changing back 'user' to 'User' on line 7

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const user = require('../models/User')
 
 module.exports = function (passport) {
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() }, (err, user) => {
+    user.findOne({ email: email.toLowerCase() }, (err, user) => {
       if (err) { return done(err) }
       if (!user) {
         return done(null, false, { msg: `Email ${email} not found.` })
