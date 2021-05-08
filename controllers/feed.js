1bf62b93a2be8c331a6de00ec44c6bf64a83c6cc
Shara Crosslin <sharacrosslin@gmail.com>
2021-05-08 13:18:18 -0700

    commented functions and features for clarity

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,8 @@
+// imports the model file so our feed knows how to structure the data (image, cloudinaryId, opinion, completed, userId, likes, and createdAt...title may be uncommented later)
 const Opinion = require("../models/Opinion");
 
 module.exports = {
+  // this function looks for all documents in our opinion collection and puts them into an array
   feedDisplay: async (req, res) => {
     try {
       const allOpinions = await Opinion.find({}).sort({ likes: -1 });
@@ -9,17 +11,18 @@ module.exports = {
       console.log(err);
     }
   },
+  // this function matches the opinionId between the like button pressed and its parent element, then accesses the 'likes' property and increments the value by 1
   likeOpinion: async (req, res) => {
     try {
       await Opinion.findOneAndUpdate(
         { _id: req.body.opinionId },
         {
-          $inc: { likes: 1 },
+          $inc: { likes: 1 }
         }
       );
       res.json({ message: "successfully liked" });
     } catch (err) {
       console.log(err);
     }
-  },
+  }
 };
--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -5,11 +5,7 @@ require("dotenv").config({ path: "./config/.env" });
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-<<<<<<< HEAD
   api_secret: process.env.API_SECRET
-=======
-  api_secret: process.env.API_SECRET,
->>>>>>> a18241bee3149e1ed483196324d2318e7d6c861f
 });
 
 module.exports = cloudinary;
