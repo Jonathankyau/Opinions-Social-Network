d77622329034564294da6bc951ede59e91ad1943
Jonathan Yau <jonathankyau@gmail.com>
2021-05-12 01:33:17 -0700

    changes link

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ require("./config/passport")(passport);
 connectDB();
 
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public/"));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
