3be37e45e010dabc66e073bd7776613d0e35e31c
Jeremy Seckinger <jtseck@aol.com>
2021-05-11 22:54:52 -0700

    Testing server.js line 24 change

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ require("./config/passport")(passport);
 connectDB();
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("/public/"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
